f562a70372e87504669f41a1c388146eb7a8365b
Justin Ho <justinho515@gmail.com>
2019-10-25 16:38:56 -0700

    Update table creation statements after schema updates.

--- app/tables.py
+++ app/tables.py
@@ -15,33 +15,34 @@ tables['users'] = (
     "   UNIQUE KEY `ID_UNIQUE` (`id`),"
     "   UNIQUE KEY `username_UNIQUE` (`username`),"
     "   UNIQUE KEY `email_UNIQUE` (`email`)"
-    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='	'"
+    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='	';"
 )
 
 tables['questions'] = (
     "CREATE TABLE `questions` ("
     "   `question_id` int(11) NOT NULL AUTO_INCREMENT,"
-    "   `user_id` int(11) NOT NULL,"
+    "   `user_id` int(11) DEFAULT NULL,"
     "   `title` varchar(250) NOT NULL,"
     "   `date_created` datetime NOT NULL,"
     "   `date_updated` datetime NOT NULL,"
-    "   `is_anonymous` tinyint(4) NOT NULL DEFAULT '1',"
+    "   `is_anonymous` tinyint(4) DEFAULT '1',"
     "   `source` varchar(25) NOT NULL DEFAULT 'SideEffects App',"
-    "   PRIMARY KEY (`id`),"
-    "   UNIQUE KEY `id_UNIQUE` (`id`),"
+    "   `num_comments` int(11) NOT NULL DEFAULT '0',"
+    "   PRIMARY KEY (`question_id`),"
+    "   UNIQUE KEY `id_UNIQUE` (`question_id`),"
     "   UNIQUE KEY `title_UNIQUE` (`title`)"
-    ") ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci"
+    ") ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
 )
 
 tables['comments'] = (
     "CREATE TABLE `comments` ("
     "   `id` int(11) NOT NULL AUTO_INCREMENT,"
-    "   `user_id` int(11) NOT NULL,"
+    "   `user_id` int(11) DEFAULT NULL,"
     "   `question_id` int(11) NOT NULL,"
     "   `comment_text` varchar(500) NOT NULL,"
-    "   `is_anonymous` tinyint(4) NOT NULL,"
+    "   `is_anonymous` tinyint(4) DEFAULT '1',"
     "   `source` varchar(45) NOT NULL DEFAULT 'SideEffects App',"
     "   PRIMARY KEY (`id`),"
     "   UNIQUE KEY `id_UNIQUE` (`id`)"
-    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci"
+    ") ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
 )
