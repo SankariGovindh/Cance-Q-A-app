4751880ccf6c6035b752e5382dfc5bdd429f6122
Justin Ho <justinho515@gmail.com>
2019-10-25 17:33:21 -0700

    Update column names.

--- app/tables.py
+++ app/tables.py
@@ -3,26 +3,23 @@
 
 tables = {}
 tables['users'] = (
-    "CREATE TABLE `users` ("
-    "   `id` int(11) NOT NULL AUTO_INCREMENT,"
-    "   `username` varchar(45) NOT NULL,"
-    "   `email` varchar(45) NOT NULL,"
-    "   `password` varchar(45) NOT NULL,"
-    "   `date_created` datetime NOT NULL,"
-    "   `question_history` json DEFAULT NULL,"
-    "   `cancer_history` json DEFAULT NULL,"
-    "   PRIMARY KEY (`id`),"
-    "   UNIQUE KEY `ID_UNIQUE` (`id`),"
-    "   UNIQUE KEY `username_UNIQUE` (`username`),"
-    "   UNIQUE KEY `email_UNIQUE` (`email`)"
-    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='	';"
+    "CREATE TABLE `comments` ("
+    "   `comment_id` int(11) NOT NULL AUTO_INCREMENT,"
+    "   `user_id` int(11) DEFAULT NULL,"
+    "   `question_id` int(11) NOT NULL,"
+    "   `comment_text` varchar(750) NOT NULL,"
+    "   `is_anonymous` tinyint(4) DEFAULT '1',"
+    "   `source` varchar(45) NOT NULL DEFAULT 'SideEffects App',"
+    "   PRIMARY KEY (`comment_id`),"
+    "   UNIQUE KEY `id_UNIQUE` (`comment_id`)"
+    "   ) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
 )
 
 tables['questions'] = (
     "CREATE TABLE `questions` ("
     "   `question_id` int(11) NOT NULL AUTO_INCREMENT,"
     "   `user_id` int(11) DEFAULT NULL,"
-    "   `title` varchar(250) NOT NULL,"
+    "   `title` varchar(500) NOT NULL,"
     "   `date_created` datetime NOT NULL,"
     "   `date_updated` datetime NOT NULL,"
     "   `is_anonymous` tinyint(4) DEFAULT '1',"
@@ -36,13 +33,13 @@ tables['questions'] = (
 
 tables['comments'] = (
     "CREATE TABLE `comments` ("
-    "   `id` int(11) NOT NULL AUTO_INCREMENT,"
+    "   `comment_id` int(11) NOT NULL AUTO_INCREMENT,"
     "   `user_id` int(11) DEFAULT NULL,"
     "   `question_id` int(11) NOT NULL,"
-    "   `comment_text` varchar(500) NOT NULL,"
+    "   `comment_text` varchar(750) NOT NULL,"
     "   `is_anonymous` tinyint(4) DEFAULT '1',"
     "   `source` varchar(45) NOT NULL DEFAULT 'SideEffects App',"
-    "   PRIMARY KEY (`id`),"
-    "   UNIQUE KEY `id_UNIQUE` (`id`)"
-    ") ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
+    "   PRIMARY KEY (`comment_id`),"
+    "   UNIQUE KEY `id_UNIQUE` (`comment_id`)"
+    "   ) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
 )
