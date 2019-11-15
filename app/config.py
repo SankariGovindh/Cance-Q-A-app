fb8705abff7e489cc8de567fe9ed982cc04c7d9f
Justin Ho <justinho515@gmail.com>
2019-11-15 14:34:34 -0800

    Add getting secret key parameter

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ def create_config_object(env_setting):
     new_config = Config()
     with env.prefixed(env_setting):
         new_config.SQLALCHEMY_DATABASE_URI = env.str("SQLALCHEMY_DATABASE_URI")
-        new_config.DEBUG = env.bool("FLASK_DEBUG", default=True)
+        new_config.FLASK_DEBUG = env.bool("FLASK_DEBUG", default=True)
         new_config.FLASK_ENV = env.str("ENV", "development")
         new_config.TESTING = env.bool("TESTING", default=False)
     return new_config
@@ -18,7 +18,7 @@ def create_config_object(env_setting):
 
 class Config:
     """Set Flask configuration variables."""
-    # SECRET_KEY=env.str("SECRET_KEY") # IMPORTANT!!!
+    SECRET_KEY=env.str("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = env.str(
         "SQLALCHEMY_DATABASE_URI", default=env.str("DEV_SQLALCHEMY_DATABASE_URI")
