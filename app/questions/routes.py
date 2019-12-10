6adf773bfaeaec4b502322b6843d54f0d2094427
Justin Ho <justinho515@gmail.com>
2019-12-09 19:04:36 -0800

    Update documentation for questions/routes

--- app/questions/routes.py
+++ app/questions/routes.py
@@ -8,6 +8,8 @@ from ..models import Question, Comment, User
 from datetime import datetime
 from flask_login import login_required
 from .. import db
+
+# 'search' method imports
 import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -36,11 +38,6 @@ questions_bp = Blueprint("questions_bp", __name__)
 @login_required
 def add_question():
     """Add a question to the database.
-
-    Precondition(s):
-    - user_id is not None
-    - question_text is not None
-    - is_anon is not None
     """
 
     # get query parameters
@@ -59,8 +56,8 @@ def add_question():
                             user_id=current_user.user_id,
                             username=current_user.username,
                             message="This question already exists!"), code=302)
-        
-        # make sure title is not null
+
+        # make sure title is not None
         if title is None:
             title = ""
 
@@ -68,7 +65,7 @@ def add_question():
         if source is None or len(source) == 0:
             source = "SideEffects App" # default source
 
-        # create new question object
+        # create new Question object
         new_question = Question(user_id=user_id,
                         title=title,
                         content=content,
@@ -77,14 +74,14 @@ def add_question():
                         is_anonymous=is_anon,
                         source=source,
                         num_comments=0)
-        db.session.add(new_question) # adds a new question to the database
+        db.session.add(new_question) # adds a new Question to the database
         db.session.commit() # commit all changes to the database
 
         return redirect(url_for("questions_bp.get_recent_questions",
                         user_id=current_user.user_id,
                         username=current_user.username,
                         message="Question successfully created!"), code=302)
-        
+
     return redirect(url_for("questions_bp.get_recent_questions",
                     user_id=current_user.user_id,
                     username=current_user.username,
@@ -94,9 +91,7 @@ def add_question():
 @questions_bp.route("/get_question", methods=["GET"])
 @login_required
 def get_question():
-    """Return a question from the database with id 'question_id' in JSON format.
-
-    Returns a JSON response containing the question text of the given question_id.
+    """Return JSON response with information of a question.
     """
 
     # get query parameters
@@ -129,26 +124,29 @@ def get_question():
 @questions_bp.route("/get_recent_questions", methods=["GET"])
 @login_required
 def get_recent_questions():
-    """Return a JSON object of the top 10 most recently updated questions FAQs."""
+    """Return a JSON response of the 10 most recently updated questions.
+    """
 
-    # get arguments from calling function
+    # get query parameters
     user_id = request.args.get("user_id")
     username = request.args.get("username")
 
-    # get top 10 questions from database (what metric defines a FAQ?)
-    recent_qs = Question.query.order_by(desc(Question.date_updated)).limit(10).all()
+    # get most recently updated questions from database
+    count = 10
+    recent_questions = Question.query.order_by(desc(Question.date_updated)).limit(count).all()
 
     # construct response
     response = []
-    for question in recent_qs:
+    for question in recent_questions:
 
         question_id = question.question_id
+
         # check if the question is from Facebook
         if question.user_id == 0:
-            q_username = "Facebook"
+            question_username = "Facebook"
         else:
-            q_user = User.query.filter_by(user_id=question.user_id).first()
-            q_username = q_user.username
+            question_user = User.query.filter_by(user_id=question.user_id).first()
+            question_username = question_user.username
 
         # get comments associated with the question_id
         comments = Comment.query.filter_by(question_id=question_id).all()
@@ -166,7 +164,7 @@ def get_recent_questions():
                 # get the user for that comment
                 comment_user = User.query.filter_by(user_id=comment.user_id).first()
                 comment_username.append(comment_user.username)
-            comment_user_id.append(comment.user_id)            
+            comment_user_id.append(comment.user_id)
             comment_content.append(comment.content)
             comment_source.append(comment.source)
             comment_is_anon.append(comment.is_anonymous)
@@ -178,7 +176,7 @@ def get_recent_questions():
             "username": username,
             "id": question.question_id,
             "question_user_id": question.user_id,
-            "question_username": q_username,
+            "question_username": question_username,
             "question_title": question.title,
             "question_date_updated": question.date_updated,
             "question_source": question.source,
@@ -202,14 +200,16 @@ def get_recent_questions():
 @questions_bp.route("/delete_question", methods=["DELETE"])
 @login_required
 def delete_question():
+    """Delete a question from the database.
+    """
     # get query parameters
     question_id = request.args.get("question_id")
 
     # check if question_id is None or not
     if question_id is not None:
         # get the question that needs to be deleted
-        delete_q = Question.query.filter_by(question_id=question_id).first()
-        db.session.delete(delete_q)
+        question = Question.query.filter_by(question_id=question_id).first()
+        db.session.delete(question)
 
         db.session.commit()
         return redirect(url_for("questions_bp.get_recent_questions",
@@ -226,6 +226,8 @@ def delete_question():
 @questions_bp.route("/update_question", methods=["PUT"])
 @login_required
 def update_question():
+    """Update a question in the database.
+    """
     # get query parameters
     question_id = request.args.get("question_id")
     new_title = request.args.get("title")
@@ -235,13 +237,14 @@ def update_question():
     # check for missing required variables
     if question_id is not None and new_content is not None and is_anon is not None:
         # get the question that needs to be updated
-        update_q = Question.query.filter_by(question_id=question_id).first()
+        question = Question.query.filter_by(question_id=question_id).first()
+
         # update the following fields of the question
         if new_title is not None:
-            update_q.title = new_title
-        update_q.content = new_content
-        update_q.date_updated = datetime.now()
-        update_q.is_anonymous = is_anon
+            question.title = new_title
+        question.content = new_content
+        question.date_updated = datetime.now()
+        question.is_anonymous = is_anon
 
         db.session.commit()
         return redirect(url_for("questions_bp.get_recent_questions",
@@ -258,19 +261,20 @@ def update_question():
 @questions_bp.route("/get_question_history", methods=["POST"])
 @login_required
 def get_question_history():
-    """Return a JSON object of all the questions asked by the user with user_id"""
+    """Return a JSON response of all the questions asked by a user.
+    """
 
     # get query parameters
     user_id = request.args.get("user_id")
 
     if user_id:
         # get all history questions for that user
-        all_question = Question.query.filter_by(user_id=user_id).all()
+        all_questions = Question.query.filter_by(user_id=user_id).all()
 
         # construct response
         response = []
 
-        if len(all_question) == 0:
+        if len(all_questions) == 0:
             response = [{
                 "username": "",
                 "user_id": user_id,
@@ -290,15 +294,17 @@ def get_question_history():
                 "comment_is_anon": [],
                 "comment_user_id": []
             }]
+
         for question in all_question:
 
             question_id = question.question_id
+
             # check if the question is from Facebook
             if question.user_id == 0:
-                q_username = "Facebook"
+                question_username = "Facebook"
             else:
-                q_user = User.query.filter_by(user_id=question.user_id).first()
-                q_username = q_user.username
+                question_user = User.query.filter_by(user_id=question.user_id).first()
+                question_username = question_user.username
 
             # get comments associated with the question_id
             comments = Comment.query.filter_by(question_id=question_id).all()
@@ -311,14 +317,14 @@ def get_question_history():
             for comment in comments:
                 # check if the comment is from Facebook group
                 if comment.user_id == 0:
-                    c_username = "Facebook"
+                    comment_username = "Facebook"
                 else:
                     # get the user for that comment
-                    c_user = User.query.filter_by(user_id=comment.user_id).first()
-                    c_username = c_user.username
+                    comment_user = User.query.filter_by(user_id=comment.user_id).first()
+                    comment_username = comment_user.username
 
                 comment_user_id.append(comment.user_id)
-                comment_username.append(c_username)
+                comment_username.append(comment_username)
                 comment_content.append(comment.content)
                 comment_source.append(comment.source)
                 comment_is_anon.append(comment.is_anonymous)
@@ -330,7 +336,7 @@ def get_question_history():
                 "user_id": user_id,
                 "id": question.question_id,
                 "question_user_id": question.user_id,
-                "question_username": q_username,
+                "question_username": question_username,
                 "question_title": question.title,
                 "question_date_updated": question.date_updated,
                 "question_source": question.source,
@@ -357,9 +363,13 @@ def get_question_history():
 
 @questions_bp.route("/search", methods=["POST"])
 @login_required
-def search():    
-    query = request.args.get("query")    
+def search():
+    """Returns a JSON response containing the most relevant questions for a user's
+    search query.
+    """
+    query = request.args.get("query")
     TOP_K = 5 # get top 5 most relevant questions
+
     # get file path of .pkl and .joblib files
     dirname = os.path.dirname(os.path.abspath(__file__))
     pkl_file = os.path.join(dirname, "vectors.pkl")
@@ -371,16 +381,16 @@ def search():
     # preprocess query
     query = preProcess(query)
     cosine_similarities = cosine_similarity(tfidf_matrix, tfidf_vectorizer.transform([' '.join(query)])).reshape(-1)
-    cosine_similarities = cosine_similarities[cosine_similarities > 0.0]    
+    cosine_similarities = cosine_similarities[cosine_similarities > 0.0]
     if not cosine_similarities.tolist():
         # raise Exception('No similar questions found!!')
         return redirect(url_for("questions_bp.get_recent_questions",
                                 message="No similar questions found."), code=302)
     top_k_max_indices = cosine_similarities.argsort()[-TOP_K:][::-1]
     top_k_question_ids = np.array(ids)[top_k_max_indices].tolist() # top_k_question_ids is a list of integers
-    
+
     # generate response object containing the top k most relevant questions and their comments
-    response = []    
+    response = []
     for question_id in top_k_question_ids:
 
         question = Question.query.filter_by(question_id=question_id).first()
@@ -431,62 +441,68 @@ def search():
             "comment_is_anon": comment_is_anon,
             "comment_user_id": comment_user_id,
             "comment_username": comment_username
-        })    
-        
+        })
+
     return jsonify(response), 200
-    # return redirect(url_for("questions_bp.get_recent_questions", 
-    #                         user_id=current_user.user_id, 
-    #                         username=current_user.username,
-    #                         message="Most relevant questions retrieved."), code=302)
 
 
 @questions_bp.route("/train_model", methods=["GET"])
-# @login_required
 def train_model():
+    """Trains the model that the 'search' function uses to find the most relevant
+    questions for a user's search query.
+    """
 
-    return redirect(url_for("questions_bp.get_recent_questions", 
-                            user_id=current_user.user_id, 
+    return redirect(url_for("questions_bp.get_recent_questions",
+                            user_id=current_user.user_id,
                             username=current_user.username,
                             message="Model training complete."), code=302)
 
 
-# function to preprocess a string query for NLP
-def preProcess(query):             
-             
-    ##removing punctuation 
-    def removePunct(non_punctuation):        
-        non_punctuation = "".join([char for char in query if char not in string.punctuation]) ##if character not part of the string.punctuation list store as prossedData 
-        return non_punctuation  
+# function to preprocess a string query for 'search'
+def preProcess(query):
+    """Preprocess a string query for the 'search' function.
 
-    ##tokenizing as separate words  
+    Args:
+        query (string): the string query to preprocess
+    
+    Returns:
+        the processed string query
+    """
+
+    # removing punctuation
+    def removePunct(non_punctuation):
+        non_punctuation = "".join([char for char in query if char not in string.punctuation]) # if character not part of the string.punctuation list store as prossedData
+        return non_punctuation
+
+    # tokenizing as separate words
     def tokenize(tokens):
-        tokens = re.split('\W+',processedQuery) ##tokenizing every word and separating the same using commas 
-        return tokens 
+        tokens = re.split('\W+',processedQuery) # tokenizing every word and separating the same using commas
+        return tokens
 
-    ##removing stopwords 
+    # removing stopwords
     def removeStopword(cleanData):
-        stopwords = nltk.corpus.stopwords.words('english') ##storing all the stopwords for the English language 
+        stopwords = nltk.corpus.stopwords.words('english') # storing all the stopwords for the English language
         cleanData = [word for word in cleanData if word not in stopwords]
-        return cleanData 
+        return cleanData
 
-    ##stemming 
+    # stemming
     def stemmer(currentData):
         stemming = nltk.PorterStemmer()
         stemmedData = [stemming.stem(word) for word in currentData]
         return stemmedData
 
-    ##lemmatizing 
+    # lemmatizing
     def lemmatizing(currentData):
         lemma = nltk.WordNetLemmatizer()
         lemData = [lemma.lemmatize(word) for word in currentData]
-        return lemData 
+        return lemData
 
-    ##part-of-speeching 
+    # part-of-speech tagging
     def posTagging(processedQuery):
-        tag = nltk.pos_tag([i for i in processedQuery if i]) 
+        tag = nltk.pos_tag([i for i in processedQuery if i])
         parsed = [word for word,pos in tag if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'JJ' or pos == 'VB')]
         return parsed
-    
+
     processedQuery = removePunct(query)
     processedQuery = tokenize(processedQuery)
     processedQuery = removeStopword(processedQuery)
